ad51a9c7f13f0f5246f00982194e463d85fcc0aa
Helen Efebe <efebehelen95@gmail.com>
2023-09-21 22:57:23 +0100

    drag-drop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,73 @@
+// import Image from "next/image";
+// import { getUnsplashImages, searchImages } from "@/lib/utils";
+// import { useEffect, useState } from "react";
+
+// export async function getServerSideProps() {
+//   const data = await getUnsplashImages();
+
+//   return { props: { data } };
+// }
+
+// export default function Home({ data }) {
+//   const [images, setImages] = useState(data);
+//   const [searchQuery, setSearchQuery] = useState("");
+
+//   useEffect(() => {
+//     if (!searchQuery) return;
+
+//     if (searchQuery === "") {
+//       setImages(data);
+//     } else {
+//       async function search() {
+//         const data = await searchImages(searchQuery);
+//         return data.results;
+//       }
+//       search().then((res) => setImages(res));
+//     }
+//   }, [searchQuery]);
+
+//   return (
+//     <>
+//       <header className="p-5">
+//         <div className="flex justify-between items-center">
+//           <h1>Image Gallery</h1>
+
+//           <div>
+//             <form className="border-2 border-gray-800 rounded-md px-3 py-1 w-full max-w-[525px] mx-auto flex justify-between items-center">
+//               <input
+//                 type="text"
+//                 name="query"
+//                 className="bg-transparent placeholder:text-xs outline-none w-full"
+//                 placeholder="Search"
+//                 id="search"
+//                 onChange={(e) => setSearchQuery(e.target.value)}
+//               />
+//               {/* <label htmlFor="search" className="text-white">
+//               <SearchIcon />
+//             </label> */}
+//             </form>
+//           </div>
+//         </div>
+//       </header>
+
+//       <main>
+//         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
+//           {images.map((image) => (
+//             <div key={image.id} className="relative w-full h-96 max-w-md">
+//               <Image
+//                 alt={image.alt_description}
+//                 src={image.urls.regular}
+//                 fill
+//                 className="w-full h-full object-cover"
+//               />
+//             </div>
+//           ))}
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
+
 import Image from "next/image";
 import { getUnsplashImages, searchImages } from "@/lib/utils";
 import { useEffect, useState } from "react";
@@ -11,6 +81,7 @@ export async function getServerSideProps() {
 export default function Home({ data }) {
   const [images, setImages] = useState(data);
   const [searchQuery, setSearchQuery] = useState("");
+  const [draggedImage, setDraggedImage] = useState(null);
 
   useEffect(() => {
     if (!searchQuery) return;
@@ -26,34 +97,59 @@ export default function Home({ data }) {
     }
   }, [searchQuery]);
 
+  const handleDragStart = (image) => {
+    setDraggedImage(image);
+  };
+
+  const handleDragOver = (image) => {
+    if (!draggedImage || image.id === draggedImage.id) return;
+
+    const newImages = images.filter((img) => img.id !== draggedImage.id);
+    const draggedIndex = images.findIndex((img) => img.id === draggedImage.id);
+    const dropIndex = images.findIndex((img) => img.id === image.id);
+
+    newImages.splice(dropIndex, 0, draggedImage);
+    setImages(newImages);
+    setDraggedImage(null);
+  };
+
   return (
     <>
       <header className="p-5">
         <div className="flex justify-between items-center">
           <h1>Image Gallery</h1>
+        </div>
 
-          <div>
-            <form className="border-2 border-gray-800 rounded-md px-3 py-1 w-full max-w-[525px] mx-auto flex justify-between items-center">
-              <input
-                type="text"
-                name="query"
-                className="bg-transparent placeholder:text-xs outline-none w-full"
-                placeholder="Search"
-                id="search"
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-              {/* <label htmlFor="search" className="text-white">
+        <div>
+          {" "}
+          <form className="border-2 border-gray-800 rounded-md px-3 py-1 w-full max-w-[525px] mx-auto flex justify-between items-center">
+            {" "}
+            <input
+              type="text"
+              name="query"
+              className="bg-transparent placeholder:text-xs outline-none w-full"
+              placeholder="Search"
+              id="search"
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            {/* <label htmlFor="search" className="text-white">
               <SearchIcon />
             </label> */}
-            </form>
-          </div>
+          </form>
         </div>
+        {/* </div> */}
       </header>
 
       <main>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
           {images.map((image) => (
-            <div key={image.id} className="relative w-full h-96 max-w-md">
+            <div
+              key={image.id}
+              className="relative w-full h-96 max-w-md"
+              draggable
+              onDragStart={() => handleDragStart(image)}
+              onDragOver={() => handleDragOver(image)}
+            >
               <Image
                 alt={image.alt_description}
                 src={image.urls.regular}
